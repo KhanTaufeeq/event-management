24d9b5e734ea6ad1a307d53abcc20c655f6308e9
KhanTaufeeq <taufeeqyouth@gmail.com>
2025-09-10 17:40:14 +0530

    updated github-actions-demo.yaml

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -71,7 +71,7 @@ export const Home = () => {
             </SwiperSlide>
             <div className="absolute inset-0 bg-black/70 z-1">
                 <h1 className="absolute top-50 left-50 text-9xl text-[#D4AF37]/70">Grandeur <br /> Gatherings</h1>
-                <p className="absolute bottom-45 left-55 text-5xl italic">We create the memories you'll hold forever</p>
+                <p className="absolute bottom-45 left-55 text-5xl italic">We create the memories you will hold forever</p>
             </div>
         </Swiper>
     );
